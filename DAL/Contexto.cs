72213969aadeff79935a2b117d27d05798de51f0
Maria Isabel Alberto Del Orbe <maria_alberto1@ucne.edu.do>
2020-03-11 16:11:43 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Parcial2_Maria.Entidades;
+
+namespace Parcial2_Maria.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        
+            optionsBuilder.UseSqlite(@"Data Source= DATA\RParcial2.db");
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
